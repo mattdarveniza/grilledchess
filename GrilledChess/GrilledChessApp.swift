9426f05afb8448fa985ff02572d2b49ef65cee08
Matt Darveniza <matt.darveniza@navis.com>
2021-10-30 17:06:15 +1100

    Initial Commit

---
+++ GrilledChess/GrilledChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrilledChessApp.swift
+//  GrilledChess
+//
+//  Created by Matt Darveniza on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GrilledChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
